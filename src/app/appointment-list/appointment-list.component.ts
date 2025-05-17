6e36304779e4c33093b85a4144176ef746122072
alfredryangerero <alfredrrg-v@crimsonlogic.com>
2025-05-17 09:32:46 +0800

    storing appointmentsi in the localstorage

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -23,12 +23,14 @@ export class AppointmentListComponent {
       this.appointments.push(newAppointment)
       this.newAppointmentTitle="";
       this.newAppointmentDate= new Date();
-      
+
+      localStorage.setItem("appointments",JSON.stringify(this.appointments))      
     } 
   }
 
   deleteAppointment(index: number){
     this.appointments.splice(index,1)
+    localStorage.setItem("appointments",JSON.stringify(this.appointments))   
   }
 
 }
